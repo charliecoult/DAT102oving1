e04821dd91e1436b53200bcace83047dcdf5cf27
Glenn Arnold Barosen <glennbarosen@gmail.com>
2019-01-20 05:57:03 +0100

    div metoder i CDArkiv

--- src/no/hvl/dat102/CDArkiv.java
+++ src/no/hvl/dat102/CDArkiv.java
@@ -7,10 +7,13 @@ public class CDArkiv implements CDArkivADT {
 	// Objektvariabler
 	private CD[] cdTabell;
 	private int antall;
+	private int lengde;
 	
 	// Konstruktør
 	public CDArkiv(int antall) {
-		cdTabell = new CD[antall];
+		cdTabell = new CD[lengde];
+		this.antall = antall;
+		
 	}
 
 	// Getters/Setters
@@ -33,39 +36,102 @@ public class CDArkiv implements CDArkivADT {
 
 	//Metoder
 	
+	//
 	@Override
 	public CD[] hentCdTabell() {
-		// TODO Auto-generated method stub
-		return null;
+		return cdTabell;
 	}
+	
+	// Legger til CD i tabell
 	@Override
 	public void leggTilCd(CD nyCd) {
-		// TODO Auto-generated method stub
-		
+		this.antall = 0;
+		for (int i = 0; i < cdTabell.length; i++) {
+			if (cdTabell[i] == null) {
+				cdTabell[i] = nyCd;
+			}
+			antall++;
+		}
 	}
+	
+	// Sletter CD gitt ved nummer på CD'en
 	@Override
 	public boolean slettCd(int nummer) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean slettet = false;
+		for (int i = nummer; i < antall() - 1 ; i++) {
+			cdTabell[i] = cdTabell[i + 1];
+		}
+		cdTabell[antall() - 1] = null;
+		slettet = true;
+		
+		return slettet;			
 	}
+
+	// Returnerer CD'er med en gitt tittel
 	@Override
 	public CD[] sokTittel(String substring) {
-		// TODO Auto-generated method stub
-		return null;
+		// Finner størrelse på tabell med resultater
+		int newSize = 0;
+		for (int i = 0; i < cdTabell.length; i++) {
+			if (cdTabell[i].getTittel() == substring) {
+				newSize++;
+			}
+		}
+		// Lager resultattabell og legger inn resultater
+		int index = 0;
+		CD[] sokTittelTabell = new CD[newSize];
+			for (int i = 0; i < cdTabell.length; i++) {
+				if (cdTabell[i].getTittel() == substring) {
+					cdTabell[i] = sokTittelTabell[index];
+					index++;
+				}
+			}
+			return sokTittelTabell;
 	}
+	
+	// Returnerer CD'er av en gitt artist
 	@Override
 	public CD[] sokArtist(String substring) {
-		// TODO Auto-generated method stub
-		return null;
+		// Finner størrelse på tabell med resultater
+		int newSize = 0;
+		for (int i = 0; i < cdTabell.length; i++) {
+			if (cdTabell[i].getArtist() == substring) {
+				newSize++;
+			}
+		}
+		// Lager resultattabell og legger inn resultater
+		int index = 0;
+		CD[] sokArtistTabell = new CD[newSize];
+			for (int i = 0; i < cdTabell.length; i++) {
+				if (cdTabell[i].getArtist() == substring) {
+					cdTabell[i] = sokArtistTabell[index];
+					index++;
+				}
+			}
+			return sokArtistTabell;
 	}
+	
+	// Returnerer antall CD'er av en gitt sjanger
 	@Override
 	public int antallSjanger(Sjanger sjanger) {
-		// TODO Auto-generated method stub
-		return 0;
+		int antallCD = 0;
+		for (int i = 0; i < cdTabell.length; i++) {
+			if (cdTabell[i].getSjanger() == sjanger) {
+				antallCD++;
+			}
+		}
+		return antallCD;
 	}
+	
+	// Returnerer antall CD'er i tabellen
 	@Override
 	public int antall() {
-		// TODO Auto-generated method stub
-		return 0;
+		int antallCD = 0;
+		for (int i = 0; i < cdTabell.length; i++) {
+			if (cdTabell[i] != null) {
+				antallCD++;
+			}
+		}
+		return antallCD;
 	}
 }
